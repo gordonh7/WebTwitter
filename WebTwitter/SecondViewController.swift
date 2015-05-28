1b5e37301f32762be6d1dc818dd6b6eed0ccdbdd
Gordon Hee <GordonH@Gordons-MacBook-Pro.local>
2015-05-28 08:22:22 -0700

    Initial Commit

---
+++ WebTwitter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WebTwitter
+//
+//  Created by Gordon Hee on 5/28/15.
+//  Copyright (c) 2015 Gordon Hee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
