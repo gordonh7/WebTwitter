a8f47b006647074ac7a02ff4608bb97b08778bdb
Gordon Hee <GordonH@Gordons-MacBook-Pro.local>
2015-05-30 17:45:11 -0700

    Integrated TwitterKit

--- WebTwitter/SecondViewController.swift
+++ WebTwitter/SecondViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import TwitterKit
+
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Twitter.sharedInstance().logInGuestWithCompletion { (session: TWTRGuestSession!, error: NSError!) in
+            Twitter.sharedInstance().APIClient.loadTweetWithID("20") { (tweet: TWTRTweet!, error: NSError!) in
+                self.view.addSubview(TWTRTweetView(tweet: tweet))
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
